0a5e726bd0f8abbcf2c540e2fa1ae915fed4f333
Petra Bosilj <bosilj.p@gmail.com>
2020-02-12 11:53:35 +0000

    Added extra steps

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 wget https://github.com/pbosilj/scikit-image-maxtree/archive/build.zip
 sudo apt-get install unzip
-unzip build.zip -d .
+unzip build.zip
+mv scikit-image-maxtree-build/* .
+rm -r scikit-image-maxtree-build
+rm build.zip
